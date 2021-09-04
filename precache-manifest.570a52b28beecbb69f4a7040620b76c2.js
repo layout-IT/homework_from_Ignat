387c41d85f7ce6088ddc957411a8cd46c0282ec6
layout-IT <77637040+layout-IT@users.noreply.github.com>
2021-09-04 19:15:29 +0300

    Updates

---
+++ precache-manifest.570a52b28beecbb69f4a7040620b76c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93cd75bfd9292681ad1436d79ce69ee7",
+    "url": "/homework_from_Ignat/index.html"
+  },
+  {
+    "revision": "9ba7d65cab845832994b",
+    "url": "/homework_from_Ignat/static/css/main.06ef0116.chunk.css"
+  },
+  {
+    "revision": "191f0f249db8dd43cf45",
+    "url": "/homework_from_Ignat/static/js/2.403f94b8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/homework_from_Ignat/static/js/2.403f94b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ba7d65cab845832994b",
+    "url": "/homework_from_Ignat/static/js/main.9ff0c4ec.chunk.js"
+  },
+  {
+    "revision": "73dad9f909fcac333df8",
+    "url": "/homework_from_Ignat/static/js/runtime-main.2c7bf3c3.js"
+  }
+]);
